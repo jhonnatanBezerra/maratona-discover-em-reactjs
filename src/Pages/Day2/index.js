58a8c0f8d9d27b399de30cf95ba63931016757ec
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2021-01-22 22:04:44 +0000

    starting the Modal

--- src/Pages/Day2/index.js
+++ src/Pages/Day2/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { FiArrowUpCircle, FiArrowDownCircle, FiDollarSign, FiMinusCircle } from 'react-icons/fi';
+import { IoAddCircleOutline } from 'react-icons/io5'
 
 import './styles.css';
 
 export const Day2 = () => {
+
+
+  const modal = () => {
+    return (
+      <>
+        <div className="modal-overlay">
+          <div className="modal">
+            <div className="content">
+              Oi
+          </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+
   return (
     <>
       <header className="day2">
@@ -10,28 +29,39 @@ export const Day2 = () => {
       </header>
       <main className="containers-day2">
         <section className="balance-day2">
-          <h2>Balanço</h2>
+          <h2 className="sr-only">Balanço</h2>
 
           <div className="cards-day2">
-            <h3>Entradas</h3>
+            <h3>
+              <span>Entradas</span>
+              <FiArrowUpCircle className="income-icon" />
+            </h3>
             <p>R$ 5.000,00</p>
           </div>
 
           <div className="cards-day2">
-            <h3>Saídas</h3>
+            <h3>
+              <span>Saídas</span>
+              <FiArrowDownCircle className="expanse-icon" />
+
+            </h3>
             <p>R$ 2.000,00</p>
           </div>
 
           <div className="cards-day2 total-day2">
-            <h3>Total</h3>
+            <h3>
+              <span>Total</span>
+              <FiDollarSign />
+
+            </h3>
             <p>R$ 3.000,00</p>
           </div>
 
         </section>
 
         <section className="transaction-day2">
-          <h2>Transações</h2>
-
+          <h2 className="sr-only">Transações</h2>
+          <a href="#"><IoAddCircleOutline /> Nova Transação</a>
           <table className="data-table-day2">
 
             <thead>
@@ -40,6 +70,7 @@ export const Day2 = () => {
                 <th>Descrição</th>
                 <th>Valor</th>
                 <th>Data</th>
+                <th></th>
               </tr>
 
             </thead>
@@ -49,18 +80,21 @@ export const Day2 = () => {
                 <td className="description-day2">Luz</td>
                 <td className="expanse-day2">- R$ 500,00</td>
                 <td className="date-day2"> 23/12/2021</td>
+                <td><FiMinusCircle /></td>
               </tr>
 
               <tr>
                 <td className="description-day2">Criação Website</td>
                 <td className="income-day2">R$ 5.000,00</td>
                 <td className="date-day2"> 23/12/2021</td>
+                <td><FiMinusCircle /></td>
               </tr>
 
               <tr>
                 <td className="description-day2">Internet</td>
                 <td className="expanse-day2">- R$ 200,00</td>
                 <td className="date-day2"> 23/12/2021</td>
+                <td><FiMinusCircle /></td>
               </tr>
             </tbody>
 
@@ -69,6 +103,7 @@ export const Day2 = () => {
         </section>
 
       </main>
+
       <footer className="container-day2">
         <p>dev.finance$</p>
       </footer>
